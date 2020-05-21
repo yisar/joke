15ff3d5c6a9592b8cf0d15af3143c9d378073ff7
132yse <1533540012@qq.com>
2020-05-21 20:06:47 +0800

    add

--- src/parser.rs
+++ src/parser.rs
@@ -2,13 +2,26 @@ use lexer;
 
 #[derive(Debug)]
 pub struct Parser {
-    pub lexer:lexer: lexer::Lexer,
+    pub lexer: lexer::Lexer,
 }
 
 impl Parser {
-    pub fn new(code:String) -> Parser{
-        Parser{
-            lexer:lexer::Lexer::new(code),
+    pub fn new(code: String) -> Parser {
+        Parser {
+            lexer: lexer::Lexer::new(code),
+        }
+    }
+    pub fn next(&mut self) -> Result<None,()>{
+        self.read_script()
+    }
+}
+
+impl Parser {
+    pub fn read_script(&mut self) -> Result<None,()>{
+        let mut items = vec![];
+
+        loop {
+            unimplemented!();
         }
     }
 }
