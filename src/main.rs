8b604700d1b9155b0dcee5c6f2e45ed63560c01c
132yse <1533540012@qq.com>
2020-05-21 19:39:21 +0800

    add cli

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate clap;
+use clap::{App, Arg};
+
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+fn main() {
+    let app = App::new("joke").arg(Arg::with_name("file").index(1));
+    let app_matches = app.clone().get_matches();
+    if let Some(filename) = app_matches.value_of("file") {
+        run(filename);
+    }
+}
+
+fn run(name: &str) {
+    let mut body = String::new();
+    match OpenOptions::new().read(true).open(name) {
+        Ok(mut ok) => ok.read_to_string(&mut body).ok().expect("cannot read file"),
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
+}
